c93eadffb4dbbff9c2a0c6d6a9e1ca90d37f0018
JunningPeng <junning.peng@warwick.ac.uk>
2024-01-09 15:48:31 +0000

    Update R code and dataset

--- Week 1.R
+++ Week 1.R
@@ -1,1 +1,24 @@
+# Set corret working directory
+setwd("/Users/pengjunning/Desktop/Teaching/IB9MH Analytics for Behavioural Research/IB9MH")
+
+#import data 
+library(readr)
+rawdata <- read_csv("fake_pension_and_debt.csv")
+View(rawdata)
+
+# load pacakges 
+library(tidyverse)
+library(ggplot2)
+library(esquisse)
+library(sjPlot)
+
+# Draw some plots 
+esquisse::esquisser(rawdata)
+
+tab_model(
+  summary(
+    lm(debt ~ months.enrolled + age + sex, data = rawdata)
+    ), show.se = TRUE, show.stat = TRUE)
+
+
 
