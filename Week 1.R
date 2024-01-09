64f1755e5e8430502afc75677908c092a78368bb
JunningPeng <junning.peng@warwick.ac.uk>
2024-01-09 16:21:01 +0000

    Treating DV as categorical variable

--- Week 1.R
+++ Week 1.R
@@ -11,14 +11,26 @@ library(tidyverse)
 library(ggplot2)
 library(esquisse)
 library(sjPlot)
+library(afex)
 
 # Draw some plots 
 esquisse::esquisser(rawdata)
 
+# Treating months enrolled as continuous variable
 tab_model(
   summary(
     lm(debt ~ months.enrolled + age + sex, data = rawdata)
     ), show.se = TRUE, show.stat = TRUE)
+
+# Treating months enrolled as categorical variable
+rawdata$months.enrolled <- factor(rawdata$months.enrolled, levels = c(19,21,22,24,25,27,28,29,31,32,33,34,41))
+# now it has become a categorical with 13 levels 
+set_default_contrasts()
+tab_model(
+  summary(
+    lm(debt ~ months.enrolled + age + sex, data = rawdata)
+  ), show.se = TRUE, show.stat = TRUE)
+
 
 
 
